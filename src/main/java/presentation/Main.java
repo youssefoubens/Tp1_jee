89407dd958caa00704292de246f8cca8e61b75f9
youssefoubens <oubenssaidyoussef@gmail.com>
2025-02-24 10:22:36 +0000

    instanciation dynamique

--- src/main/java/presentation/Main.java
+++ src/main/java/presentation/Main.java
@@ -5,14 +5,37 @@ import Dao.IDaoImpl1;
 import metier.Metier;
 import metier.MetierImp1;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
-
+    static private List<String> calsses = new ArrayList<>();
     public static void main(String[] args)
     {
-        IDao iDao = new IDaoImpl1();
-        Metier metier = new MetierImp1(iDao);
+        try
+        {
+            Scanner scanner = new Scanner(new File("config.txt"));
+            while(scanner.hasNext())
+            {
+                calsses.add(scanner.nextLine());
+            }
 
-        metier.calcule();
+
+            Class Dao = Class.forName(calsses.get(0));
+            Class Metier1 = Class.forName(calsses.get(1));
+
+            IDao dao = (IDao) Dao.getConstructor().newInstance();
+            Metier metier = (MetierImp1) Metier1.getConstructor(IDao.class).newInstance(dao);
+
+            metier.calcule();
+
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
